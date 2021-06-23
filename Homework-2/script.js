65226f3b66d63054367d4dd5e9388fd9884c7eda
MVigor47 <mytsko47@gmail.com>
2021-06-24 02:01:46 +0300

    variables

---
+++ Homework-2/script.js
@@ -0,0 +1,30 @@
+// Sup wrld
+
+let firstVariable;
+
+firstVariable = "Sup wrld"
+
+console.log(firstVariable)
+
+
+let customerAge = "47";
+console.log(customerAge);
+
+let userName = "George";
+console.log(userName);
+
+let userSurname = "Natan";
+console.log(userSurname);
+
+
+const newUser = "user 322"
+let shoppingList = "1"
+let onlineStatus = "brb"
+let workerSalary = "2$"
+let cursorLocation = "topLeft"
+
+let presentHobby = "streaming"
+// 
+presentHobby = "sleeping";
+console.log(presentHobby);
+
