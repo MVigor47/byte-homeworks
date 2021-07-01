60d4cab1b44336810af7d594550a7dc36968b20c
MVigor47 <mytsko47@gmail.com>
2021-07-02 00:07:47 +0300

    Homework3

---
+++ Homework-3/script.js
@@ -0,0 +1,61 @@
+//Задание 1
+
+// const firstNumber = Number(prompt("Назовите первое число"));
+// const secondNumber = Number(prompt("Назовите второе число"));
+// if  ( firstNumber > secondNumber){
+//     alert (firstNumber + ' больше ' + secondNumber + ' :)');
+// } if  ( firstNumber < secondNumber){
+//     alert (firstNumber + ' меньше ' + secondNumber + ' :)');
+// } if  ( firstNumber === secondNumber){
+//     alert ("Ничия :)");
+// }            
+
+
+//Задание 2
+
+// const firstNumber = Number(prompt("Первое число"));
+// const secondNumber = Number(prompt("Второе число"));
+// const mathOperation = prompt("Математическая операция")
+// switch (mathOperation){
+//     case "+":
+//         alert (firstNumber + secondNumber);
+//         break;
+//         case "-":
+//         alert (firstNumber - secondNumber);
+//         break;
+//         case "*":
+//         alert (firstNumber * secondNumber);
+//         break;
+//         case "/":
+//         alert (firstNumber / secondNumber);
+//         break;
+//     default:
+//         alert ("Я не калькулятор")    
+
+// }
+
+
+//Задание 3
+
+const minChildAge = 12;
+const maxChildAge = 18;
+const minOldAge = 60;
+const maxOldAge = 80;
+const visitorAge = Number(prompt("Ваш возраст"));
+const parentsPermit = confirm("Разрешение родителей?");
+
+if (visitorAge >= maxChildAge && visitorAge <= maxOldAge){
+    alert ("Вам можно");
+} else if (visitorAge >= minChildAge && visitorAge <= 80 || parentsPermit) {
+    alert ("Вам можна")
+}else {
+    alert ("Вам нельзя")
+}
+
+
+// // } else if (visitorAge <= maxChildAge){
+// }
+
+
+
+
