69a057503e99eeea6ab4078df7de7ef5ef32defe
MVigor47 <mytsko47@gmail.com>
2021-07-08 20:20:42 +0300

    a few more HW

---
+++ Homework-4/sript.js
@@ -0,0 +1,62 @@
+// Завдання 1
+
+// function sum (a,b){
+//     return a + b
+// }
+
+// const sumRes = sum(234, 965)
+// console.log(sumRes)
+
+// const sum = (a, b) => a + b
+
+// const sumRes2 = sum(228,147)
+// console.log(sumRes2)
+
+// const sumRes3 = sum(123,147)
+// console.log(sumRes3)
+
+// const sumRes4 = sum(222,147)
+// console.log(sumRes4)
+
+// const sumRes5 = sum(10034204350,2354354326662)
+// console.log(sumRes5)
+
+// const sumRes6 = sum(12,23)
+// console.log(sumRes6)
+
+
+// Завдання 2 
+
+// function yourInfo (){
+//     userName = prompt("Ваше имя?");
+//     userAge = Number(prompt("Ваш возраст"));
+//     if (userAge >= 30){
+//         return alert ("Здравствуйте," + userName)
+//     } else {
+//         return alert ("Привет," + userName)
+//     }
+// }
+
+// const userGreeting = yourInfo();
+
+// Завдання 3
+
+function pow(a, b = 2){
+    return a**b
+} 
+
+const numberInPower = pow(25);
+console.log (numberInPower);
+
+// function pow(){
+//     number = Number(prompt("Число"));
+//     power = Number(prompt("Степень"));
+//     const result = number ** power
+//     console.log(result);
+//         if (power === ""){
+//             return number ** 2
+//         }
+// }
+
+// pow()
+
